3085712b35245b0edf71a3de76704085652d5a81
el0quenz <el0quenz92@gmail.com>
2020-01-31 21:49:14 +0100

    Initial commit to host the App

---
+++ deploy/scripts/forecast.js
@@ -0,0 +1,24 @@
+const key = "ewNjS1iXPtfzNlIByqlJG7RU4tabiAQ1";
+
+//get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  //await function makes javascript wait until the promise returns
+  const data = await response.json();
+
+  return data[0];
+};
+
+//get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
